c921c7d4166ab5c62fd10c30666a43ad9629c858
ngreene84 <113942096+ngreene84@users.noreply.github.com>
2023-01-31 09:25:37 -0500

    Null print statement test

--- main.c
+++ main.c
@@ -5,5 +5,9 @@ static char text[] = "Hello Sonatype Lift !";
 int main() {
     char * printme = text;
     printf("%s\n",printme); 
+    char * printStatement;
+    printf("%s\n",printStatement);
+    
+    printf("%s\n", "Testing hard-coded print statement");
     return 0;
 }
