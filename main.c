924a2075d6f5541cd0ee7bc29764b5ed1597c8b6
ngreene84 <113942096+ngreene84@users.noreply.github.com>
2023-01-31 18:00:00 -0500

    Update main.c

--- main.c
+++ main.c
@@ -5,5 +5,7 @@ static char text[] = "Hello Sonatype Lift !";
 int main() {
     char * printme = text;
     printf("%s\n",printme); 
+    char * printStatement;
+    printf("%s\n",printStatement);
     return 0;
 }
